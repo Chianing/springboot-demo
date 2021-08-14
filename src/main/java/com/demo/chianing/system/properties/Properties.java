50094ea1335facbcdb95207f056cfc1b0c7bc8bb
Chianing <chianing9527@gmail.com>
2021-08-15 00:31:10 +0800

    custom configuration

--- src/main/java/com/demo/chianing/system/properties/Properties.java
+++ src/main/java/com/demo/chianing/system/properties/Properties.java
@@ -1,20 +1,40 @@
 package com.demo.chianing.system.properties;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 /**
- * 变量
+ * 全局属性
  */
 @Component
 public class Properties {
 
-    public static SystemProperties systemProperties;
+    /**
+     * 系统属性
+     */
+    public static SystemProperties system;
 
     @Autowired
     private void setSystem(SystemProperties systemProperties) {
-        Properties.systemProperties = systemProperties;
+        Properties.system = systemProperties;
     }
 
+    @Data
+    @Component
+    @ConfigurationProperties(prefix = "system")
+    public static class SystemProperties {
+        /**
+         * 系统管理员
+         */
+        private List<String> admins;
+        /**
+         * 当前激活的环境
+         */
+        private String activeEnv;
+    }
 
 }
